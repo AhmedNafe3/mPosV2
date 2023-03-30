e80af3ba0a7259f9e34cc7ccfb9bab10587128d5
Ahmed Nafea <jiftun.it@gmail.com>
2023-03-30 15:13:11 +0300

    add last code update

--- app/src/main/java/com/example/mposv2/MainActivity.java
+++ app/src/main/java/com/example/mposv2/MainActivity.java
@@ -1,41 +1,35 @@
 package com.example.mposv2;
 
 import android.Manifest;
-import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.view.View;
+import android.widget.Toast;
 
 import com.example.mposv2.databinding.ActivityMainBinding;
 import com.mpos.mpossdk.api.MPOSService;
 import com.mpos.mpossdk.api.MPOSServiceCallback;
 
+import java.util.HashMap;
+
 public class MainActivity extends AppCompatActivity {
     ActivityMainBinding binding;
+    HashMap<String, String> response;
+    MPOSService mposService;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
-
         setContentView(binding.getRoot());
-        MPOSService.getInstance(this).checkExistence(new MPOSServiceCallback() {
-            @Override
-            public void onComplete(int i, String s, Object o) {
-                binding.statusTxt.setText(s);
-                binding.connectBut.setVisibility(View.GONE);
-            }
 
-            @Override
-            public void onFailed(int i, String s) {
-                binding.connectBut.setVisibility(View.VISIBLE);
+        mposService = MPOSService.getInstance(this);
+        checkDeviceAvailability();
 
-            }
-        });
         binding.connectBut.setOnClickListener(v -> {
             if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
                 if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
@@ -48,10 +42,65 @@ public class MainActivity extends AppCompatActivity {
 
         });
         binding.nextBut.setOnClickListener(v -> {
-            startActivity(new Intent(this, PayActivity.class));
+            checkLastTransactionResult();
         });
     }
 
+    public void checkLastTransactionResult() {
+        //startActivity(new Intent(getApplicationContext(), PayActivity.class));
 
+        mposService.getLastTransactionResult(new MPOSServiceCallback() {
+            @Override
+            public void onComplete(int i, String s, Object result) {
+                HashMap<String, String> response = mposService.parseTransactionResponse((String) result);
+                binding.orderNumber.setText(response.get("AdditionalData"));
+                binding.textApproverCode.setText(response.get("ApprovalCode"));
+                binding.textTERMINALSTATUSCODE.setText(response.get("TerminalStatusCode"));
+                binding.textAmount.setText(response.get("Amount"));
+                binding.textRRN.setText(response.get("RRN"));
+            }
 
+            @Override
+            public void onFailed(int i, String s) {
+                Toast.makeText(getApplicationContext(), s, Toast.LENGTH_SHORT).show();
+
+            }
+        });
+    }
+
+    public void checkDeviceAvailability() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.BLUETOOTH_CONNECT) == PackageManager.PERMISSION_DENIED) {
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.S) {
+                ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 2);
+                return;
+            } else {
+                mposService.connectToDevice(new MPOSServiceCallback() {
+                    @Override
+                    public void onComplete(int i, String s, Object o) {
+                        binding.statusTxt.setText(s);
+                        binding.connectBut.setVisibility(View.GONE);
+                    }
+
+                    @Override
+                    public void onFailed(int i, String s) {
+                        binding.connectBut.setVisibility(View.VISIBLE);
+                    }
+                });
+            }
+        } else {
+            mposService.connectToDevice(new MPOSServiceCallback() {
+                @Override
+                public void onComplete(int i, String s, Object o) {
+                    binding.statusTxt.setText(s);
+                    binding.connectBut.setVisibility(View.GONE);
+                }
+
+                @Override
+                public void onFailed(int i, String s) {
+                    binding.connectBut.setVisibility(View.VISIBLE);
+
+                }
+            });
+        }
+    }
 }
--- app/src/main/java/com/example/mposv2/PayActivity.java
+++ app/src/main/java/com/example/mposv2/PayActivity.java
@@ -2,6 +2,7 @@ package com.example.mposv2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 import com.example.mposv2.databinding.ActivityPayBinding;
 import com.mpos.mpossdk.api.MPOSService;
@@ -9,6 +10,7 @@ import com.mpos.mpossdk.api.MPOSServiceCallback;
 
 public class PayActivity extends AppCompatActivity {
     ActivityPayBinding binding;
+    MPOSServiceCallback callback;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,13 +19,13 @@ public class PayActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         binding.button3.setOnClickListener(v -> {
-            checkLastTransactionResult();
+            requestTransaction();
         });
+
     }
 
-
     private void requestTransaction() {
-        String xmlRequest = "<TransactionRequest>" + "<Command>SALE</Command>" + "<Amount>100</Amount>" + "<Cashback></Cashback>" + "<MaskedPAN></MaskedPAN>" + "<PrintFlag>01</PrintFlag>" + "<Phone></Phone>" + "<Email></Email>" + "<UserId></UserId>" + "<DeviceId></DeviceId>" + "<RRN></RRN>" + "<AuthCode></AuthCode>" + "<AdditionalData></AdditionalData>" + "</TransactionRequest>";
+        String xmlRequest = "<TransactionRequest>" + "<Command>SALE</Command>" + "<Amount>100</Amount>" + "<Cashback></Cashback>" + "<MaskedPAN></MaskedPAN>" + "<PrintFlag>01</PrintFlag>" + "<Phone></Phone>" + "<Email></Email>" + "<UserId></UserId>" + "<DeviceId></DeviceId>" + "<RRN></RRN>" + "<AuthCode></AuthCode>" + "<AdditionalData>1020304050</AdditionalData>" + "</TransactionRequest>";
 
         MPOSService.getInstance(this).startTransaction(xmlRequest, new MPOSServiceCallback() {
             @Override
@@ -40,17 +42,5 @@ public class PayActivity extends AppCompatActivity {
         });
     }
 
-    public void checkLastTransactionResult() {
-        MPOSService.getInstance(this).getLastTransactionResult(new MPOSServiceCallback() {
-            @Override
-            public void onComplete(int i, String mes, Object result) {
-                requestTransaction();
-            }
 
-            @Override
-            public void onFailed(int i, String s) {
-
-            }
-        });
-    }
 }
